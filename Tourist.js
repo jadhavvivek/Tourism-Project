fd8c45952a590d66773bfdac94646895977e85ce
Vivek Jadhav <vivekjadhav391999@gmail.com>
2024-06-30 19:41:29 +0530

    Tourism

--- Tourist.js
+++ Tourist.js
@@ -18,7 +18,7 @@ const destinations = [
     { 
         id: 'destination1', 
         title: 'Ajanta and Ellora Caves', 
-        imgSrc: 'Ajanta 1.jpg', 
+        imgSrc: 'Images/Ajanta 1.jpg', 
         description: 'Ajanta and Ellora caves, considered to be one of the finest examples of ancient rock-cut caves, are located near Aurangabad in Maharashtra, India. Ajanta and Ellora caves complex is adorned with beautiful sculptures, paintings, and frescoes and include Buddhist monasteries, Hindu and Jain temples. The Ajanta caves are 29 in number and were built between the 2nd century BC and 6th century AD, whereas the Ellora Caves are more spread out and 34 in number and dates to the period between 6th and 11th Century AD.', 
         location: 'Maharashtra, India',
         weather: 'Sunny with occasional clouds',
@@ -26,7 +26,7 @@ const destinations = [
     { 
         id: 'destination2', 
         title: 'Alibaug', 
-        imgSrc: 'alibag 1.jpg', 
+        imgSrc: 'Images/alibag 1.jpg', 
         description: 'Alibaug Tourism Alibaug (also spelled as Alibag) is a small coastal town in the Konkan region of Maharashtra, known for its beaches, villas, and beautiful scenery. Alibag is very popular as a weekend getaway from the cities of Mumbai and Pune. Water sports in Alibaug like parasailing, banana boat and jet ski and speed boating are quite popular during the dry season. Most tourist families spend at least one night in Alibag with extending to multiple nights.  ', 
         location: 'Maharashtra, India',
         weather: 'Moderate temperature, occasional rain',
@@ -34,7 +34,7 @@ const destinations = [
     { 
         id: 'destination3', 
         title: 'Khandala', 
-        imgSrc: 'Khandala 1.jpg', 
+        imgSrc: 'Images/Khandala 1.jpg', 
         description: 'Khandala Tourism Khandala is a popular hill station in Maharashtra, perched on the foothills of Sahyadri in the Western Ghats. Khandala provides for a perfect weekend respite for the Mumbai population, with its scenic valleys, grassy hills, serene lakes and misty waterfalls. It is one of the twin hill stations - Lonavala and Khandala. The place immortalized by the popular Bollywood song, "Aati Kya Khandala", is a must-visit destination for everyone looking for beautiful nature, pleasant weather and the feeling of mist in the air. The natural beauty of Khandala blooms to its zenith and reveals a magical charm during the monsoons. Its meandering waterfalls and glittering lakes attributes to the majestic look an imposing ambience.', 
         location: 'Maharashtra, India',
         weather: 'Cool with fog in mornings and evenings',
@@ -42,7 +42,7 @@ const destinations = [
     { 
         id: 'destination4', 
         title: 'Lonawala', 
-        imgSrc: 'Lonawala 1.jpg', 
+        imgSrc: 'Images/Lonawala 1.jpg', 
         description: 'Lonavala Tourism Situated in the Sahyadri range of the Western Ghats close to Pune and Mumbai, Lonavala is the most visited hill station in Maharashtra and the place to be during monsoons. With lots of waterfalls, lakes and hills around, it is a popular destination for camping, trekking and other adventure sports', 
         location: 'Maharashtra, India',
         weather: 'Pleasant, occasional showers',
@@ -50,7 +50,7 @@ const destinations = [
     { 
         id: 'destination5', 
         title: 'Mahabaleshwar', 
-        imgSrc: 'mahabaleshwar 1.jpg', 
+        imgSrc: 'Images/mahabaleshwar 1.jpg', 
         description: 'Mahabaleshwar is a hill station located in the Western Ghats, in Satara district of Maharashtra. Apart from its strawberries, Mahabaleshwar is also well known for its numerous rivers, magnificent cascades and majestic peaks. It is among the most sought after weekend getaways from Pune & Mumbai, located about 120km south-west of Pune and 285km from Mumbai.', 
         location: 'Maharashtra, India',
         weather: 'Cool and misty, light rain expected',
@@ -58,7 +58,7 @@ const destinations = [
     { 
         id: 'destination6', 
         title: 'Bhandaradara', 
-        imgSrc: 'bhandardara 1.jpg', 
+        imgSrc: './Images/bhandardara 1.jpg', 
       description: "Bhandardara, a hill station nestled in the Sahyadri ranges of Maharashtra, has all the nature's blessings a place can get. The lush greenery, the humble waterfalls and surrounded by high mountains makes for the perfect holiday spot for city dwellers. Situated in the Ahmednagar district of Maharashtra, Bhandardara is 117 km. from Mumbai and is easily accessible through roadways. This little village is packed with so many attractions coupled with the serenity of the nature that it makes for an adventurous weekend to run away from the hustle and bustle of the cities.",
         location: 'Maharashtra, India',
         weather: 'Moderate with sunshine and clear skies',
